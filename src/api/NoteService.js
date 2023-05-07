e5ac3d74aecb926d37c9f813110968af2659287b
Taha-NABIL <81980315+Taha-NABIL@users.noreply.github.com>
2023-05-07 16:19:01 +0200

    Update NoteService.js

--- src/api/NoteService.js
+++ src/api/NoteService.js
@@ -1,1 +1,21 @@
+import Note from './Model.js';
 
+async function create({ name, text }) {
+  const note = new Note({name, text });
+  const newnote = await note.save();
+  return newnote;
+}
+
+function read(id) {
+  return Note.findById(id);
+}
+
+function list() {
+  return Note.find();
+}
+
+export default {
+  create,
+  list,
+  read,
+};

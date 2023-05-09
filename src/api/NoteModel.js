88d5523a28efaedd2089e0af1127b8b46069ff4b
Taha-NABIL <81980315+Taha-NABIL@users.noreply.github.com>
2023-05-09 16:41:36 +0200

    del NoteModel.js

--- src/api/NoteModel.js
+++ src/api/NoteModel.js
@@ -1,9 +1,1 @@
-import { Schema, model } from 'mongoose';
 
-const NoteSchema = new Schema({
-  name: String,
-  text: String,{
-  timestamps: true,
-});
-
-export default model('Note', FileSchema);
